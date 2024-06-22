5fa3c2a886217eba1c253a38fb9a40564a3fc3d7
Anurag12133 <anurajput62616@gmail.com>
2024-06-22 19:54:34 +0530

    Navbar-inserted

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
-
+import "@/app/css/mainPage.css";
+import { NavbarDemo } from "./components/Navbar";
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <NavbarDemo />
+    </div>
+  );
 }
